88b5defb57a2d1e0c6a09496dbce3167762d8ab9
Jeffrey Pfaff <Jeffreypfaff@msn.com>
2013-11-04 11:01:04 +0100

    Getting one result now from the ldap of Topicus.

--- php/message/Employee.php
+++ php/message/Employee.php
@@ -5,9 +5,20 @@ require_once('../config/config.php');
 $ldap = ldap_connect(LDAP_LOCATION) or die("Could not connect to LDAP server.");
 
 if ($ldap) {
-    //$bind = ldap_bind($conn, LDAP_USER, LDAP_PASS);
+    $bind = ldap_bind($ldap, LDAP_USER, LDAP_PASS);
 
+    $base_dn = "OU=TopicusUsers, OU=Topicus, DC=topicus, DC=local";
 
+    $attributes = array("name", "mail");
+//CN=topicusonderwijs
+
+    $email ="thijs.elferink@topicus.nl";
+
+    $result = ldap_search($ldap, $base_dn,  "(cn=*)") or die ("Error in search query");
+
+    $info = ldap_get_entries($ldap, $result);
+
+    echo json_encode($info);
 }
 
 
