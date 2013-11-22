a50c9a9cfd4ca1b6499016345568ed7e98e61d4b
Jeffrey Pfaff <Jeffreypfaff@msn.com>
2013-11-22 10:11:27 +0100

    XML content of one calendar is collected.

--- php/widget/agenda/agenda.php
+++ php/widget/agenda/agenda.php
@@ -1,14 +1,11 @@
 <?php
 
-require_once('../../config/config.php');
-require_once('../../lib/RestServer.php');
+//$user = 'topicus.dashboard@gmail.com';
+//$pass = 'Deventer11';
 
-class Agenda {
-
-    public static function main() {
-        return 'hi';
-    }
-}
-
-$rest = new RestServer('Agenda');
-$rest->handle();
+$googleURL = "https://www.google.com/calendar/feeds/mr5t4iqhiu81dh0cdq040cruc4%40group.calendar.google.com/private-ac4baac1a7cfab33824e2b02261b7b81/basic";
+//$googleURL = "http://www.google.com/calendar/feeds/$userid/private-$magicWord/basic?alt=json";
+//$url = "http://www.google.com/calendar/feeds/developer-calendar@google.com/public/full?alt=json";
+$data = file_get_contents($googleURL);
+$feed = simplexml_load_string($data);
+echo json_encode($feed);
